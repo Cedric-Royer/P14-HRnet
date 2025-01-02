fac334aa488814c08ec33ac7e4f04e5a9da8bb06
Cedric Royer <cedricroyerpro@gmail.com>
2025-01-02 18:12:20 +0100

    import legacy

---
+++ src/TitlePage.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+function TitlePage() {
+    return (
+      <>
+        <div className="title">
+          <h1>HRnet</h1>
+        </div>
+      </>
+    )
+  }
+  
+  export default TitlePage
