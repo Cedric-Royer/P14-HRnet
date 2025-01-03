fc617607000deb456a470c6b5ca54b5056a7c2d0
Cedric Royer <cedricroyerpro@gmail.com>
2025-01-03 22:32:27 +0100

    add tailwind classes to form

--- src/components/CreateEmployee.jsx
+++ src/components/CreateEmployee.jsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import TitlePage from "./TitlePage";
 import LabeledInput from "./FormElements/LabeledInput";
 import LabeledSelect from "./FormElements/LabeledSelect";
-import Example from "./formTemplate";
 
 function CreateEmployee() {
   function saveEmployee() {
@@ -40,21 +39,87 @@ function CreateEmployee() {
     document.getElementById("confirmation").style.display = "none";
   }
 
+  const handleSubmit = (event) => {
+    event.preventDefault(); 
+    console.log("Form submitted!"); 
+  };
+
   return (
     <>
       <TitlePage title="HRnet" />
       <div className="container">
         <Link to="/employee-list">View Current Employees</Link>
-        <button onClick={saveEmployee}>Save</button>
       </div>
 
-      <div id="confirmation" className="modal-container" style={{ display: 'none' }}>
+    <form 
+        onSubmit={handleSubmit}
+        className="mx-auto max-w-7xl px-4 py-3 sm:px-6 sm:py-4 lg:px-8"
+        action="#"
+        id="create-employee"
+    >
+      <div className="space-y-1">
+      <h2 className="text-base/7 font-semibold text-gray-900">Create Employee</h2>
+        <div className="border-b border-gray-900/10 pb-12">
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="sm:col-span-3">
+                <LabeledInput id="first-name" label="First Name" type="text" />
+            </div>
+
+            <div className="sm:col-span-3">
+                <LabeledInput id="last-name" label="Last Name" type="text" />
+            </div>
+
+            <div className="sm:col-span-3">
+                <LabeledInput id="date-of-birth" label="Date of Birth" type="text" />
+            </div>
+
+            <div className="sm:col-span-3">
+                <LabeledInput id="start-date" label="Start Date" type="text" />
+            </div>
+
+            <div className="col-span-full">
+                <LabeledInput id="street" label="Street" type="text" />
+            </div>
+
+            <div className="sm:col-span-2 sm:col-start-1">
+                <LabeledInput id="city" label="City" type="text" />
+            </div>
+
+            <div className="sm:col-span-2">
+                <LabeledSelect name="state" id="state" label="State" ></LabeledSelect>
+            </div>
+
+            <div className="sm:col-span-2">
+                <LabeledInput id="zip-code" label="Zip Code" type="number" />
+            </div>
+            <div className="sm:col-span-3">
+                <LabeledSelect id="department" label="Department">
+                    <option>Sales</option>
+                    <option>Marketing</option>
+                    <option>Engineering</option>
+                    <option>Human Resources</option>
+                    <option>Legal</option>
+                </LabeledSelect>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-6 flex items-center justify-end gap-x-6">
+        <button
+          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          onClick={saveEmployee}
+        >
+          Save
+        </button>
+      </div>
+    </form>
+    <div id="confirmation" className="modal-container" style={{ display: 'none' }}>
         <div className="modal">
           Employee Created!
           <span className="close" onClick={closeModal}>&times;</span>
         </div>
       </div>
-      <Example></Example>
     </>
   );
 }
