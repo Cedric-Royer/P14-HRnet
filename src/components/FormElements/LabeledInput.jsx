7d447e1c7d62ff8938a13a1583b9b92a6518addf
Cedric Royer <cedricroyerpro@gmail.com>
2025-01-04 23:57:39 +0100

    prevent submit in input

--- src/components/FormElements/LabeledInput.jsx
+++ src/components/FormElements/LabeledInput.jsx
@@ -17,7 +17,11 @@ function LabeledInput({ label, id, name, type, value, onChange }) {
             onChange={onChange}
             className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
             dateFormat="MM/dd/yyyy"
-
+            onKeyDown={(event) => {
+              if (event.key === "Enter") {
+                event.preventDefault();
+              }
+            }}
           />
         ) : (
           <input
@@ -25,8 +29,12 @@ function LabeledInput({ label, id, name, type, value, onChange }) {
             name={name}
             type={type}
             value={value}
-            onChange={onChange}
             className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+            onKeyDown={(event) => {
+              if (event.key === "Enter") {
+                event.preventDefault();
+              }
+            }}
           />
         )}
       </div>
